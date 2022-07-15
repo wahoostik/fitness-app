68fe9655dc21f9662c6c5bc7133606b5ded4810a
PACCAUD <anthony.paccaud@capgemini.com>
2022-07-15 10:43:13 +0200

    ajout du dossier assets et installation de tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <p className='text-3xl font-bold underline'>Fitness App</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
